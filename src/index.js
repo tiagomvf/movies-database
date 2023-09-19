9f70656a576f0e07e5197f7fe2047d48f67c927e
Tiago Maia <tiago.maia@trt6.jus.br>
2023-09-19 09:10:10 -0300

    Add page header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-//import '@carbon/web-components/es/components/accordion/index.js'
-// import '@carbon/web-components/es/components-react/accordion';
-import '@carbon/web-components/es/index.js';
-//import '@carbon/web-components/es/globals';
-// import './src/media/boundary/MediaCard.js'
+import '@carbon/web-components/es';
 import './media/boundary/TrendingList.js'
 import './media/boundary/PopularList.js'
 import './media/boundary/FreeToWatchList.js'
