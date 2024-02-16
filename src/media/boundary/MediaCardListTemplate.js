558f60bbbbf1adb85ea898e641edc4e9e1aaa2cc
Tiago Maia <tiago.maia@trt6.jus.br>
2024-02-16 15:36:49 -0300

    Card list centered

--- src/media/boundary/MediaCardListTemplate.js
+++ src/media/boundary/MediaCardListTemplate.js
@@ -9,7 +9,7 @@ const calcDate = ({release_date, first_air_date}) => {
 const image_host = "https://image.tmdb.org";
 export const template = (media_type, list) => html`
 
-<div style="display: flex">
+<div style="display: flex; overflow-y: auto;">
     ${
         list
         .map(({title, name, release_date, first_air_date, ...rest}) => 
@@ -21,7 +21,7 @@ export const template = (media_type, list) => html`
         ).map(({id, title, poster_path, overview, release_date}) => html`
     <cds-tile style="min-width: fit-content; padding: 0 16px 16px 16px">
     
-    <div style="width: 150px; min-width: 150; max-with: 322px">
+    <div style="width: 150px; min-width: 150px; max-width: 322px">
       <a href="/movie/${id}" title="${title}" > 
         <img loading="lazy" class="poster" style="border-radius: 8px; width: 100%"
           src="${image_host}/t/p/w220_and_h330_face${poster_path}"
