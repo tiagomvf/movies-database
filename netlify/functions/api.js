30b744c5159a4c602b992990a0eacef713167806
Tiago Maia <tiagomvf@gmail.com>
2025-03-26 15:58:06 -0300

    feat: Use api_key as environment variable

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,7 @@
-const API_KEY="a0ff4c0215cc293844471e4c38da0a84";
 exports.handler = async (request) => {
   // TODO: User header to authenticate, not apiKey path param
-
   const newUrl = new URL(request.rawUrl.replace(/https?:[\/]{2}.*[\/]api/, 'https://api.themoviedb.org'));
-  newUrl.searchParams.append('api_key', API_KEY)
+  newUrl.searchParams.append('api_key', process.env.API_KEY)
   const response = await fetch(newUrl);
   const statusCode = response.status;
   const body = await response.text();
