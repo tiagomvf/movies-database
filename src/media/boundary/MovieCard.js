2973832f04d3e7fbf1ae5a7b4a1fb11381497d49
Tiago Maia <tiagomvf@gmail.com>
2025-03-31 20:43:35 -0300

    fix: fix movie card file

--- src/media/boundary/MovieCard.js
+++ src/media/boundary/MovieCard.js
@@ -1,13 +1,12 @@
-import {html, render} from "lit-html";
+import { html, render } from "lit-html";
 
-const template = ({id, title, image_host, poster_path, release_date}) => html`
+const template = ({ id, title, image_host, poster_path, release_date }) => html`
      <cds-layer style="width: fit-content; height: fit-content; padding: 0">
      <div style="width: 150px; min-width: 150px; max-width: 322px">
        <a href="/movie/${id}" title="${title}" > 
          <img loading="lazy" class="poster" style="border-radius: 8px; width: 100%"
            src="${image_host}/t/p/w220_and_h330_face${poster_path}"
-           srcset="${image_host}/t/p/w220_and_h330_face${poster_path} 1x, ${
-    image_host}/t/p/w440_and_h660_face${poster_path} 2x" alt="">
+           srcset="${image_host}/t/p/w220_and_h330_face${poster_path} 1x, ${image_host}/t/p/w440_and_h660_face${poster_path} 2x" alt="">
        </a>
        <cds-link href="/movie/${id}" title="${title}">
          <h2 class="cds--type-heading-compact-02">${title}</h2>
@@ -27,23 +26,24 @@ class MovieListItem extends HTMLElement {
       this.view();
     });
     observer.observe(this,
-                     {attributes : true, childList : false, subtree : false})
+      { attributes: true, childList: false, subtree: false })
   }
 
   disconnectedCallback() { observer.disconnect(); }
 
   view() {
     const data = {
-      id : this.dataset.id,
-      title : this.dataset.title,
-      poster_path : this.dataset.poster_path,
-      overview : this.dataset.overview,
-      release_date : this.dataset.release_date,
-      image_host : 'https://image.tmdb.org'
-    } render(template(data), this);
+      id: this.dataset.id,
+      title: this.dataset.title,
+      poster_path: this.dataset.poster_path,
+      overview: this.dataset.overview,
+      release_date: this.dataset.release_date,
+      image_host: 'https://image.tmdb.org'
+    };
+    render(template(data), this);
   }
 
-  static get observedAttributes() { return [ "data-movie" ] }
+  static get observedAttributes() { return ["data-movie"] }
 }
 
 customElements.define("tmdb-movie-list-item", MovieListItem);
