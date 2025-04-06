68fdcb067f0422120c9540bbcd9285a051507f0a
Tiago Maia <tiagomvf@gmail.com>
2025-04-06 18:14:11 -0300

    wip: add movie details component

---
+++ src/movie/boundary/MovieDeails.js
@@ -0,0 +1,77 @@
+import { html, nothing, render } from "lit-html";
+import "../entity/type.js"
+/**
+ * @param {string} date
+ * @returns {string}
+ */
+function yearof(dateStr) {
+  const date = new Date(dateStr);
+  const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+  return year;
+}
+/**
+ * @function
+ * @param {Movie} movie
+ * @returns {any}
+ */
+const template = (movie) => html`
+<style>
+:host {
+  display: grid;
+  gap: 2em;
+  grid-template-columns: min-content auto;
+}
+
+:host > #poster {
+}
+
+#poster img {
+  border-radius: 1em;
+}
+</style>
+
+<div>
+  <div id="poster">
+    <img src=https://image.tmdb.org/t/p/w440_and_h660_face/${movie.poster_path}>
+    ${movie ? movie.id : nothing}
+  </div>
+</div>
+<div>
+  <h2>${movie.title} (${yearof(movie.release_date)})</h2>
+  <div class="tagline">
+    ${movie.tagline}
+  </div>
+  <div id="overview">
+    <h3>Overview</h3>
+    <p>${movie.overview}</p>
+  </div>
+</div>
+  `
+
+class MovieDetails extends HTMLElement {
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() { }
+
+  disconnectedCallback() { observer.disconnect(); }
+
+  view(movie) {
+    render(template(movie), this.shadowRoot);
+    this.shadowRoot.appendChild(style);
+  }
+
+  async attributeChangedCallback() {
+    const id = this.getAttribute("id");
+    const movie = await fetch(`/api/3/movie/${id}`).then(r => r.json());
+    console.log("chamou");
+    this.view(movie);
+  }
+
+  static get observedAttributes() { return ["id"] }
+}
+
+customElements.define("tmdb-movie-details", MovieDetails);
