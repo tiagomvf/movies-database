38dcd28b3487df0da21c4b785cb4379de88e9623
Tiago Maia <tiagomvf@gmail.com>
2025-04-07 21:33:28 -0300

    chore: remove unused components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import '@carbon/web-components/es';
 import './AppHeader.js'
-import './main-page/boundary/TrendingList.js'
 import './main-page/index.js'
--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,6 +1,4 @@
-import './boundary/FreeToWatchList.js';
 import './boundary/MainPage.js';
 import './boundary/MediaList.js';
 import './boundary/MovieCard.js';
-import './boundary/PopularList.js';
-import './boundary/TrendingList.js';
+import './boundary/MovieListSection.js';
